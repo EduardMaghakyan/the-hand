13bf08329366c4dbaaeb0346dd230c47877d6ce8
Eduard Maghakyan <edi.maghakyan@gmail.com>
2021-09-25 01:50:19 +0200

    WIP: handle collisions

--- gym-sorting_puzzle/gym_sorting_puzzle/envs/tiles.py
+++ gym-sorting_puzzle/gym_sorting_puzzle/envs/tiles.py
@@ -3,40 +3,56 @@ from pygame import Rect
 import numpy as np
 import random
 
+from pygame.constants import QUIT
 
 TILE_W = 90
 TILE_H = 90
+WIDTH_FACTOR = 9
+HEIGHT_FACTOR = 6
+
+class Wall(pygame.sprite.Sprite):
+    def __init__(self, color, x=0, y=0, width=0, height=0):
+        super().__init__()
+        self.image = pygame.Surface((width, height))
+        self.image.fill(color)
+        pygame.draw.rect(self.image, color, Rect(0, 0, width, height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def convert(self):
+        self.image.convert_alpha()
+
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, color, x=None, y=None):
+    def __init__(self, color, x=None, y=None, width=0, height=0):
         super().__init__()
-        self.image = pygame.Surface((TILE_W, TILE_H))
-        self.image.fill((247, 245, 242))
-        pygame.draw.circle(self.image, color, (TILE_W//2, TILE_H//2), TILE_W // 2)
+        self.image = pygame.Surface((width, height))
+        self.image.set_colorkey((0, 0, 0))
+        pygame.draw.circle(self.image, color,
+                           (width//2, height//2), width // 2)
         self.rect = self.image.get_rect()
-        if x != None:
-            self.rect.x = x
-        if y != None:
-            self.rect.y = y
+        self.rect.x = x
+        self.rect.y = y
         self.velocity = [0, 0]
+
+    def convert(self):
+        self.image.convert_alpha()
 
     def set_x(self, x):
         self.rect.x = x
-    
+
     def set_y(self, y):
         self.rect.y = y
 
-    def update(self, walls):
-        new_rect = self.rect.move(*self.velocity)
-        collide = False
-        for wall in walls:
-            if collide := wall.colliderect(new_rect):
-                break
-        if not collide:
-            self.rect = new_rect
-
-
-player = Tile(color=(247, 77, 64), x=90, y=(7*90))
+    def update(self, walls, tiles):
+        prev_rect = self.rect.copy()
+        self.rect.move_ip(*self.velocity)
+        collide_tiles = False
+        collide_walls = pygame.sprite.spritecollide(self, walls, False, pygame.sprite.collide_rect_ratio(0.9))
+        collide_tiles = pygame.sprite.spritecollide(self, tiles, False, pygame.sprite.collide_circle_ratio(0.7))
+        if len(collide_tiles) > 1 or collide_walls:
+            self.rect = prev_rect
 
 RED = (255, 18, 18)
 BLUE = (18, 164, 255)
@@ -45,58 +61,44 @@ YELLOW = (242, 231, 24)
 tiles = []
 for color in [RED, BLUE, GREEN, YELLOW]:
     for i in range(4):
-        tiles.append(Tile(color=color))
+        tiles.append(Tile(color=color, x=0, y=0, width=TILE_W, height=TILE_H))
 
 random.shuffle(tiles)
 
 WIDTH_FACTOR = 9
 HEIGHT_FACTOR = 6
+all_tiles = pygame.sprite.Group()
 for i in range(len(tiles)):
     row = i // 4
     col = i % 4
     tiles[i].set_x((col + (col + 1)) * 90)
     tiles[i].set_y((HEIGHT_FACTOR - (row + 2)) * 90)
+    all_tiles.add(tiles[i])
 
-
-# 0  1  2  3 
-# 4  5  6  7
-# 8  9  10 11
-# 12 13 14 15
+wall_color = (49, 163, 79)
+walls = pygame.sprite.Group()
+walls.add(Wall(wall_color, x=0, y=((HEIGHT_FACTOR - 1) * TILE_H), width=WIDTH_FACTOR * TILE_W, height=TILE_H))
+for i in range(9):
+    if i % 2 == 0:
+        walls.add(Wall(wall_color, i * TILE_W, TILE_H, TILE_W, (HEIGHT_FACTOR - 1) * TILE_H))
 
 class SortingPuzzle:
 
     def __init__(self) -> None:
         pygame.init()
         pygame.display.set_caption('Sort IT!')
-
-        screen_size = (WIDTH_FACTOR * TILE_W, HEIGHT_FACTOR * TILE_H)
-        self.screen = pygame.display.set_mode(screen_size)
-
+        self.screen_size = (WIDTH_FACTOR * TILE_W, HEIGHT_FACTOR * TILE_H)
+        self.screen = pygame.display.set_mode(self.screen_size)
         self.clock = pygame.time.Clock()
         self.FPS = 60
-
         self.passage_color = (247, 245, 242)
-        self.background_color = (247, 229, 200)
-        self.wall_color = (0, 0, 0)
-
-        self.cell_width = TILE_W
-        self.cell_height = TILE_H
-
         self._draw_puzzle()
 
     def _draw_puzzle(self):
-        game_surface = pygame.Surface((WIDTH_FACTOR * self.cell_width, HEIGHT_FACTOR * self.cell_height))
+        game_surface = pygame.Surface(self.screen_size)
         game_surface.fill(self.passage_color)
-
-        walls = []
-        walls.append(pygame.draw.rect(game_surface, self.background_color, Rect((0, 0), (WIDTH_FACTOR * self.cell_width, 1))))
-        walls.append(pygame.draw.rect(game_surface, self.background_color, Rect((0, (HEIGHT_FACTOR - 1) * self.cell_height), (WIDTH_FACTOR * self.cell_width, self.cell_height))))
-        for i in range(9):
-            if i % 2 == 0:
-                walls.append(pygame.draw.rect(game_surface, self.background_color, Rect((i  * self.cell_width, self.cell_height), (self.cell_width, (HEIGHT_FACTOR - 1) * self.cell_height))))
-
-        done = False
         player = None
+        done = False
         while not done:
             dt = self.clock.tick(self.FPS) / 1000
             for event in pygame.event.get():
@@ -120,14 +122,15 @@ class SortingPuzzle:
                     pos = pygame.mouse.get_pos()
                     player = [s for s in tiles if s.rect.collidepoint(pos)]
                     player = player.pop() if len(player) > 0 else None
-            
+
             self.screen.blit(game_surface, (0, 0))
             if player:
-                player.update(walls)
+                player.rect.clamp_ip(self.screen.get_rect())
+                player.update(walls, tiles)
 
-            for tile in tiles:
-                self.screen.blit(tile.image, tile.rect)
-
+            all_tiles.update(walls, tiles)
+            all_tiles.draw(self.screen)
+            walls.draw(self.screen)
             pygame.display.update()
 
 SortingPuzzle()
